473e2bbc9b9e54ae53d59a09dca01a548999933a
BCamillaCambium <bcamilla@cambium.co.il>
2025-08-27 10:08:48 +0300

    adjusted form wrapper

--- mileon-client/src/app/app.component.ts
+++ mileon-client/src/app/app.component.ts
@@ -115,7 +115,7 @@ export class AppComponent {
       let dialogData = this.dialogData;
       if (!isEdit) {
         const form = new InfrastructureForms();
-        dialogData = form.InfrastructureChipsPetForm;
+        dialogData = form.InfrastructureTicketsSourceForm;
       }
       const dialogRef = this.dialog.open(dialogComponent, {
         data: { form: dialogData, title: 'עריכת רשומה', isEdit: isEdit, isSigns: false },
