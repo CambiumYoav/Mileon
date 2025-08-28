dcaaa2fda9fac832f0a8c202062719b026418149
BCamillaCambium <bcamilla@cambium.co.il>
2025-08-28 11:49:43 +0300

    fixed pagination error

--- mileon-client/src/environments/environment.local.ts
+++ mileon-client/src/environments/environment.local.ts
@@ -4,6 +4,7 @@ import { Environment } from './environment.model';
 
 export const environment: Environment = {
   production: false,
-  apiUrl: 'https://localhost:7047/api',
+  // apiUrl: 'https://localhost:7047/api',
+  apiUrl: 'https://mileon-preprod.development.cambium.co.il/api',
   mediaUrl: 'https://d3pe2doddq0g20.cloudfront.net/'
 };
